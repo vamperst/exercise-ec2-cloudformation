bbf1015e03e296222cca3c0029c7564730569dfb
Barbosa <rafbarbo@8c85902385c3.ant.amazon.com>
2020-05-21 17:56:50 -0300

    cloudwatch agent and config

---
+++ installCloudwatchAgent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb
+sudo aws s3 cp s3://bootcamp-data-engineering-222/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo mkdir /usr/share/collectd
+sudo touch /usr/share/collectd/types.db
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
+sudo systemctl restart amazon-cloudwatch-agent
